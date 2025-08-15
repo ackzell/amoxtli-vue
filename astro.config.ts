0db7725e38176c8dc6629b796f5eb95a40e3a8a2
ackzell <ackzell@gmail.com>
2025-08-15 18:57:27 +0200

    #8: Implementing icon file
    Note the use of an expressive code plugin as well as the sytling for the changes the plugin does to the DOM. Bringing in all variables for theming the tk instance.

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,16 @@
 import tutorialkit from '@tutorialkit/astro';
+import { pluginFileIcons } from '@xt0rted/expressive-code-file-icons';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [tutorialkit()],
+  integrations: [tutorialkit({
+    // @ts-ignore
+    expressiveCodePlugins: [pluginFileIcons({
+      iconClass: 'av-icon',
+      titleClass: 'av-title',
+    })],
+  })],
 });
