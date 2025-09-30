e7817ac2a68190320d1467c4c7c8cf36f6a1597b
ackzell <ackzell@gmail.com>
2025-10-01 00:08:22 +0200

    Adding some dark styles.
    Leveraging unocss

--- uno.config.ts
+++ uno.config.ts
@@ -6,17 +6,22 @@ export default defineConfig({
   presets: [
     presetWebFonts({
       fonts: {
-        sans: 'Work+Sans:400,500,600,700',
+        sans: 'Work Sans:400,500,600,700',
+        mono: 'Space Mono:400,500,600,700',
       },
     }),
   ],
-  rules: [
-    [
-      'test-rule',
-      {
-        'background-color': 'lime !important',
-        color: 'black !important',
-      },
-    ],
-  ],
+  theme: {
+    colors: {
+      bgr: 'hsl(30, 10%, 8%)',
+      'primary-text': 'hsl(210, 29%, 80%)',
+    },
+  },
+  shortcuts: {
+    h1: 'text-3xl font-bold font-mono text-primary-text',
+    'sign-in-btn': `px-4 py-2 font-sans
+      bg-black text-light rounded
+      hover:bg-dark-400 transition-bg duration-200
+      shadow-md hover:shadow-lg`,
+  },
 });
