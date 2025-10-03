d818c391e95f9f6e5d6c57b577c586ff42184f5b
ackzell <ackzell@gmail.com>
2025-10-04 00:19:10 +0200

    Can build locally and deploy
    This means my local fork changes will be able to be deployed.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,16 @@
-import { createClient } from '@supabase/supabase-js';
+import pkg from '@supabase/supabase-js';
 
-// uses public env vars for client-side runtime
 const SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.PUBLIC_SUPABASE_KEY;
 
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+console.log('Supabase URL:', SUPABASE_URL);
+console.log('Supabase Key:', SUPABASE_KEY);
+
+let supabaseClient: pkg.SupabaseClient | null = null;
+
+if (typeof window !== 'undefined') {
+  // Initialize Supabase client only in the browser
+  supabaseClient = pkg.createClient(SUPABASE_URL, SUPABASE_KEY);
+}
+
+export { supabaseClient };
