29b6caf30c334fe83201006491da7367265be679
ackzell <ackzell@gmail.com>
2025-10-17 22:39:58 +0200

    Adding support for authenticating with Google

---
+++ src/components/GoogleLoginButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { supabaseClient } from '@/lib/supabase';
+import { ref } from 'vue';
+
+const isLoggingIn = ref(false);
+
+async function loginWithGoogle() {
+  isLoggingIn.value = true;
+  const response = await supabaseClient.auth.signInWithOAuth({
+    provider: 'google',
+    options: { redirectTo: window.location.origin + '/auth/callback' },
+  });
+
+  console.log('Google login response:', JSON.stringify(response));
+}
+</script>
+
+<template>
+  <div>
+    <div v-if="isLoggingIn" class="text-[var(--tk-text-primary)]">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+      >
+        <!-- Icon from SVG Spinners by Utkarsh Verma - https://github.com/n3r4zzurr0/svg-spinners/blob/main/LICENSE -->
+        <path
+          fill="currentColor"
+          d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="0.75s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 12 12;360 12 12"
+          />
+        </path>
+      </svg>
+    </div>
+    <button v-else class="sc-google-btn" @click="loginWithGoogle">
+      <div class="i-logos-google-icon w-5 h-5 mr-2"></div>
+      Sign in with Google
+    </button>
+  </div>
+</template>
--- uno.config.ts
+++ uno.config.ts
@@ -170,6 +170,11 @@ export default defineConfig({
       hover:scale-102 hover:shadow-2xl 
       transition-all 
       active:scale-99 active:shadow-md`,
+    'sc-google-btn': `sc-sign-in-btn flex items-center justify-center 
+      gap-2
+      hover:scale-102 hover:shadow-2xl 
+      transition-all 
+      active:scale-99 active:shadow-md`,
     'sc-custom-tooltip': `bg-[var(--background)]/60! backdrop-blur-lg! 
       rounded-xl! p-2 text-sm shadow-md z-20!
       `,
