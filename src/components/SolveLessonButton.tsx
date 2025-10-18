54c5109a816995c192b9298f138121558100eff8
ackzell <ackzell@gmail.com>
2025-10-18 23:05:35 +0200

    Creating a solve / reset button to be used inside the lesson
    Also fixing linting issues for the client:only directive in mdx files

---
+++ src/components/SolveLessonButton.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import tutorialStore from 'tutorialkit:store';
+
+export function SolveLessonButton() {
+  return <button onClick={() => tutorialStore.solve()}>Solve</button>;
+}
+
+export function ResetLessonButton() {
+  return <button onClick={() => tutorialStore.reset()}>Reset</button>;
+}
+
+export function ToggleLessonStateButton() {
+  const [solved, setSolved] = useState(false);
+  return (
+    <button
+      className="p-0 text-neutral rounded hover:text-primary-600 dark:hover:text-primary-dark-400"
+      onClick={() => {
+        if (solved) {
+          tutorialStore.reset();
+        } else {
+          tutorialStore.solve();
+        }
+        setSolved(!solved);
+      }}
+    >
+      {solved ? (
+        <div className="flex items-center gap-1">
+          <div className="i-carbon-reset inline-block"></div>
+          Reset
+        </div>
+      ) : (
+        <div className="flex items-center gap-1">
+          <div className="i-carbon-idea inline-block"></div>
+          Solve
+        </div>
+      )}
+    </button>
+  );
+}
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,18 @@
 /// <reference types="@tutorialkit/astro/types" />
 /// <reference types="astro/client" />
 
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      'client:only'?: string;
+      'client:load'?: boolean;
+      'client:idle'?: boolean;
+      'client:visible'?: boolean;
+      'client:media'?: string;
+    }
+  }
+}
+
 export type ImportMeta = {
   env: {
     PUBLIC_SUPABASE_URL: string;
