e9dbb3a4cd029f1c022d0091b6ac0268e68c1721
ackzell <ackzell@gmail.com>
2025-10-19 23:09:41 +0200

    feat: Adding anchors to the headings

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,10 @@ import vue from '@astrojs/vue';
 import tutorialkit from '@tutorialkit/astro';
 import { pluginFileIcons } from '@xt0rted/expressive-code-file-icons';
 import { defineConfig } from 'astro/config';
+import { h } from 'hastscript';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
   devToolbar: {
@@ -13,6 +16,32 @@ export default defineConfig({
       [
         rehypeExternalLinks,
         { target: '_blank', rel: ['noopener', 'noreferrer'] },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          content: [
+            h(
+              'svg',
+              {
+                width: 16,
+                height: 16,
+                viewBox: '0 0 16 16',
+                ariaHidden: 'true',
+                style:
+                  'display: inline-block; vertical-align: middle; margin-left: 0.25rem;',
+              },
+              [
+                h('path', {
+                  fill: 'currentColor',
+                  d: 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z',
+                }),
+              ]
+            ),
+          ],
+        },
       ],
     ],
   },
