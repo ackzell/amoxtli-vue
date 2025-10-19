2a9d26ad49c8afd7bdea3e1b09c618763a9654a3
ackzell <ackzell@gmail.com>
2025-10-19 23:11:10 +0200

    feat: updating component to allow for custom label

--- src/components/SolveLessonButton.tsx
+++ src/components/SolveLessonButton.tsx
@@ -9,11 +9,24 @@ export function ResetLessonButton() {
   return <button onClick={() => tutorialStore.reset()}>Reset</button>;
 }
 
-export function ToggleLessonStateButton() {
+type ToggleLessonStateButtonProps = {
+  solveStr?: string;
+  resetStr?: string;
+};
+
+export function ToggleLessonStateButton({
+  resetStr = 'Reset',
+  solveStr = 'Solve',
+  ...props
+}: ToggleLessonStateButtonProps) {
   const [solved, setSolved] = useState(false);
   return (
     <button
-      className="p-0 text-text/80 dark:text-text-dark/80 rounded hover:text-primary-600 dark:hover:text-primary-dark-400"
+      className="`
+      p-0 rounded 
+      text-text/80 dark:text-text-dark/80  
+      hover:text-primary-600 dark:hover:text-primary-dark-400
+      `"
       onClick={() => {
         if (solved) {
           tutorialStore.reset();
@@ -26,12 +39,12 @@ export function ToggleLessonStateButton() {
       {solved ? (
         <div className="flex items-center gap-1">
           <div className="i-carbon-reset inline-block"></div>
-          Reset
+          {resetStr}
         </div>
       ) : (
         <div className="flex items-center gap-1">
           <div className="i-carbon-idea inline-block"></div>
-          Solve
+          {solveStr}
         </div>
       )}
     </button>
